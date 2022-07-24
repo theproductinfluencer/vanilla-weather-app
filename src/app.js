402223dfb505f75007f9a4d6f67f2c0c8bd41845
The Product Influencer <96388680+theproductinfluencer@users.noreply.github.com>
2022-07-24 17:47:43 -0400

    Added search engine

--- src/app.js
+++ src/app.js
@@ -45,8 +45,20 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
-let city = "New York";
-let apiKey = "f5088c194f0355c3583e8278707f0c60";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "f5088c194f0355c3583e8278707f0c60";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
